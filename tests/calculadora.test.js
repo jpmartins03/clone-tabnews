161a3a130c7ab8a5cf3b3b3c823b1031141eb2c3
João Pedro Martins Montera <jpmartins1013@gmail.com>
2025-09-22 20:18:07 +0000

    test: criando um teste de teste

---
+++ tests/calculadora.test.js
@@ -0,0 +1,16 @@
+
+test("espero que 1 seja 1", () => {
+  expect(1).toBe(1);
+});
+
+// test("nome do teste", () => {
+//   console.log("Assim funciona?");
+// });
+
+// test("testando outra coisa", () => {
+//   console.log("Outro teste");
+// });
+
+// function callBackFunction(){
+//   console.log("Esta funçao esta sendo chamada?")
+// }
