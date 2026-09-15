dc3ab47e26750ef3091952be6bd43d123abf0afb
João Pedro Martins Montera <jpmartins1013@gmail.com>
2026-09-15 19:04:17 +0000

    feat: add  and

---
+++ infra/database.js
@@ -0,0 +1,19 @@
+import { Client } from "pg";
+
+async function query(queryObject) {
+  const client = new Client({
+    host: POSTGRES_HOST,
+    port: POSTGRES_PORT
+    user: POSTGRES_USER,
+    database: POSTGRES_DATABASE,
+    password: process.env.POSTGRES_PASSWORD,
+  });
+  await client.connect();
+  const result = await client.query(queryObject);
+  await client.end();
+  return result;
+}
+
+export default {
+  query: query,
+};
