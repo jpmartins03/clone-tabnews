b77db06b25684496585adc0fb3b44bcf8dba8a5c
Joäo Montera <jpmartins1013@gmail.com>
2026-09-17 11:54:16 -0400

    minor fixes

--- infra/database.js
+++ infra/database.js
@@ -2,10 +2,10 @@ import { Client } from "pg";
 
 async function query(queryObject) {
   const client = new Client({
-    host: POSTGRES_HOST,
-    port: POSTGRES_PORT
-    user: POSTGRES_USER,
-    database: POSTGRES_DATABASE,
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   });
   await client.connect();
